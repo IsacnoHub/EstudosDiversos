ff53793637324953eafde42a00df3a90d3038098
ISAC AZEVEDO PINHEIRO <isacazevedo@id.uff.br>
2026-04-08 21:33:06 -0300

    Update Lista1.java

--- POO-entregas/Lista1.java
+++ POO-entregas/Lista1.java
@@ -1,13 +1,18 @@
-package com.mycompany.olamundo;
+package com.mycompany.lista1;
 import java.util.Scanner;
-public class OlaMundo{
+public class Lista1{
+    //nomeação de classe dtem que seseguir PascalCase
+    //camelCase para métodos UPPER_SNAKE_CASE para constantes, esse eu lembrava
     public static void main(String[] args){
+        //1 - Crie um programa que peça ao usuário para digitar seu nome e, em seguida, exiba uma
+        //mensagem personalizada com o nome informado.
         Scanner teclado = new Scanner(System.in);
         System.out.print("Digite seu nome: ");
         String nome = teclado.nextLine();
         System.out.println("O nome é: "+nome+"\n");  
         
-        
+        //2 - Crie um programa que converta uma temperatura fornecida em graus Celsius para
+        //Fahrenheit usando a fórmula:
         Scanner teclado1 = new Scanner(System.in);
         System.out.print("Digite a temperatura em Celsius: ");
         float temp = teclado1.nextFloat();
@@ -16,7 +21,7 @@ public class OlaMundo{
         System.out.println(antigo+"*C equivale a: "+temp+"*F\n");
         Scanner teclado2 = new Scanner(System.in);
         
-        
+        //3 - Crie um programa que peça um número ao usuário e diga se ele é par ou ímpar.
         System.out.print("Entre com o número: ");
         int num = teclado2.nextInt();
         if (num%2 == 0){
@@ -27,7 +32,8 @@ public class OlaMundo{
         }
         
         
-        
+        //4 - Crie um programa para, a partir de um valor informado em centavos, indicar a menor
+        //quantidade de moedas que representa esse valor
         Scanner teclado3 = new Scanner(System.in);
         System.out.print("Entre com uma quantidade de dinheiro: ");
         float real = teclado3.nextFloat();
