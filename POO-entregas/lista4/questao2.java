bfee93ac2e9ac65e5ecc38d285ef8fe4f7fdc6bc
ISAC AZEVEDO PINHEIRO <isacazevedo@id.uff.br>
2026-04-08 21:44:49 -0300

    Update questao2.java

--- POO-entregas/lista4/questao2.java
+++ POO-entregas/lista4/questao2.java
@@ -1,6 +1,10 @@
-package com.mycompany.mavenproject1;
+package com.mycompany.procuraknovet;
 import java.util.Scanner;
-public class Mavenproject1 {
+public class ProcuraKNoVet {
+    /*Crie um programa para ler um número n e depois ler um vetor v com n números
+inteiros. Ao final, ler mais um número k e informar e que posições do vetor v aparece o
+número k. Caso k não exista no vetor v apresente a mensagem "Número não
+encontrado".*/
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int n = teclado.nextInt();
