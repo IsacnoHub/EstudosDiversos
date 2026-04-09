b8e8108762381e9b087051368b486ad23d30dd63
ISAC AZEVEDO PINHEIRO <isacazevedo@id.uff.br>
2026-04-08 21:40:05 -0300

    Rename class and package for interval functionality
    Refactor class name and package for interval checking.

--- POO-entregas/Lista 2/Exercicio7.java
+++ POO-entregas/Lista 2/Exercicio7.java
@@ -1,9 +1,11 @@
-package com.mycompany.mavenproject1;
+package com.mycompany.intervalo;
 import java.lang.Math;
 import java.util.Scanner;
 
-public class Mavenproject1 {
-
+public class Intervalo {
+    //Leia um número inteiro n e mais dois números (maior ou menor) que formam
+    //um intervalo. Ao final, imprima uma mensagem informando se n está antes, dentro ou
+depois do intervalo.
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Entre com os 3 nums: ");
